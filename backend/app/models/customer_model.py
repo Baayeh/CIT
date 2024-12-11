11bdab45946b6012edaf43640c42cf09ab1b19fe
Kwasi Antwi Baayeh <kabaayeh@gmail.com>
2024-12-11 19:19:39 +0000

    create db model for customers

---
+++ backend/app/models/customer_model.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from uuid import UUID, uuid4
+
+# import sqlalchemy.dialects.postgresql as pg
+from sqlmodel import Field, SQLModel
+
+
+class Customer(SQLModel, table=True):
+    __tablename__ = "customers"
+
+    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    name: str
+    phone_number: str
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime = Field(default_factory=datetime.now)
+
+    def __repr__(self):
+        return f"<Customer {self.name}>"
