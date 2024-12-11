04b9444f066a1842a631e579c879dc136a83d904
Kwasi Antwi Baayeh <kabaayeh@gmail.com>
2024-12-11 19:17:46 +0000

    configure db parameters

---
+++ backend/app/core/config.py
@@ -0,0 +1,27 @@
+import os
+from functools import lru_cache
+
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+load_dotenv()
+
+
+class DB(BaseModel):
+    host: str = os.getenv("DB_Host")
+    port: str = os.getenv("DB_PORT")
+    user: str = os.getenv("DB_USER")
+    password: str = os.getenv("DB_PASSWORD")
+    name: str = os.getenv("DB_NAME")
+
+
+class Settings(BaseSettings):
+    db: DB = DB()
+
+    model_config = SettingsConfigDict(env_nested_delimiter="__", extra="ignore")
+
+
+@lru_cache
+def get_settings():
+    return Settings()
