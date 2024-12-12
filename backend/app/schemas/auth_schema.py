4ca9dee220bd1fac200a6c62a334f976b732a89d
Kwasi Antwi Baayeh <kabaayeh@gmail.com>
2024-12-12 08:41:54 +0000

    create schema for user

--- backend/app/schemas/auth_schema.py
+++ backend/app/schemas/auth_schema.py
@@ -1,4 +1,18 @@
+from datetime import datetime
+from uuid import UUID
+
 from pydantic import BaseModel, EmailStr, Field
+
+
+class UserDetailSchema(BaseModel):
+    id: UUID
+    firstname: str = Field(min_length=3)
+    lastname: str = Field(min_length=3)
+    username: str = Field(max_length=5)
+    email: EmailStr
+    is_verified: bool
+    created_at: datetime
+    updated_at: datetime
 
 
 class UserCreateSchema(BaseModel):
