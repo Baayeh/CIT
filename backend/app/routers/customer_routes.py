a669a80d92e2c2cb7dee542bfa0b10eef052de1d
Kwasi Antwi Baayeh <kabaayeh@gmail.com>
2024-12-13 04:38:01 +0000

    inject the access token dependency in all routes

--- backend/app/routers/customer_routes.py
+++ backend/app/routers/customer_routes.py
@@ -1,8 +1,9 @@
 from typing import List
 
-from fastapi import APIRouter, status
+from fastapi import APIRouter, Depends, status
 
 from app.db.database import SessionDep
+from app.dependencies.auth_dependencies import AccessTokenBearer
 from app.schemas.customer_schema import CustomerCreateSchema, CustomerDetailsSchema
 from app.services.customer_service import CustomerService
 
@@ -11,12 +12,17 @@ from app.services.customer_service import CustomerService
 router = APIRouter()
 service = CustomerService()
 
+access_token_bearer = AccessTokenBearer()
+
+TokenDep = Depends(access_token_bearer)
+
 
 @router.get(
     "/", response_model=List[CustomerDetailsSchema], status_code=status.HTTP_200_OK
 )
-async def get_all_customers(session: SessionDep):
+async def get_all_customers(session: SessionDep, token_details=TokenDep):
     """Retrieve all books"""
+
     customers = service.get_all_customers(session)
     return customers
 
@@ -24,7 +30,9 @@ async def get_all_customers(session: SessionDep):
 @router.post(
     "/", response_model=CustomerDetailsSchema, status_code=status.HTTP_201_CREATED
 )
-async def create_customer(customer_data: CustomerCreateSchema, session: SessionDep):
+async def create_customer(
+    customer_data: CustomerCreateSchema, session: SessionDep, token_details=TokenDep
+):
     """Create a new customer"""
     new_customer = service.create_customer(customer_data, session)
     return new_customer
@@ -35,7 +43,8 @@ async def create_customer(customer_data: CustomerCreateSchema, session: SessionD
     response_model=CustomerDetailsSchema,
     status_code=status.HTTP_200_OK,
 )
-async def get_customer(customer_id: str, session: SessionDep):
+async def get_customer(customer_id: str, session: SessionDep, token_details=TokenDep):
+    """Retrieve a single customer by ID"""
     customer = service.get_customer(customer_id, session)
     return customer
 
@@ -46,12 +55,19 @@ async def get_customer(customer_id: str, session: SessionDep):
     status_code=status.HTTP_200_OK,
 )
 async def update_customer(
-    customer_id: str, customer_data: CustomerCreateSchema, session: SessionDep
+    customer_id: str,
+    customer_data: CustomerCreateSchema,
+    session: SessionDep,
+    token_details=TokenDep,
 ):
+    """Update details of a single customer"""
     updated_customer = service.update_customer(customer_id, customer_data, session)
     return updated_customer
 
 
 @router.delete("/{customer_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_customer(customer_id: str, session: SessionDep):
+async def delete_customer(
+    customer_id: str, session: SessionDep, token_details=TokenDep
+):
+    """Delete a customer"""
     service.delete_customer(customer_id, session)
