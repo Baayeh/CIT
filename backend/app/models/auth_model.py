525cb233c1db6a9123bfd20a82e575fed17cb7df
Kwasi Antwi Baayeh <kabaayeh@gmail.com>
2024-12-13 08:23:29 +0000

    update models

--- backend/app/models/auth_model.py
+++ backend/app/models/auth_model.py
@@ -1,22 +1,32 @@
 from datetime import datetime
 from uuid import UUID, uuid4
 
+import sqlalchemy.dialects.postgresql as pg
 from pydantic import EmailStr
-from sqlmodel import Field, SQLModel
+from sqlmodel import Column, Field, SQLModel
 
 
 class User(SQLModel, table=True):
     __tablename__ = "users"
 
-    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    id: UUID = Field(
+        sa_column=Column(pg.UUID, nullable=False, primary_key=True, default=uuid4)
+    )
     username: str = Field(unique=True, index=True)
     email: EmailStr = Field(unique=True, index=True)
     firstname: str
     lastname: str
+    role: str = Field(
+        sa_column=Column(pg.VARCHAR, nullable=False, server_default="user")
+    )
     password_hash: str = Field(exclude=True)
     is_verified: bool = Field(default=False)
-    created_at: datetime = Field(default_factory=datetime.now)
-    updated_at: datetime = Field(default_factory=datetime.now)
+    created_at: datetime = Field(
+        sa_column=Column(pg.TIMESTAMP, nullable=False, default=datetime.now)
+    )
+    updated_at: datetime = Field(
+        sa_column=Column(pg.TIMESTAMP, nullable=False, default=datetime.now)
+    )
 
     def __repr__(self):
         return f"<User {self.username}>"
--- backend/app/models/customer_model.py
+++ backend/app/models/customer_model.py
@@ -1,17 +1,24 @@
 from datetime import datetime
 from uuid import UUID, uuid4
 
-from sqlmodel import Field, SQLModel
+import sqlalchemy.dialects.postgresql as pg
+from sqlmodel import Column, Field, SQLModel
 
 
 class Customer(SQLModel, table=True):
     __tablename__ = "customers"
 
-    id: UUID = Field(default_factory=uuid4, primary_key=True)
+    id: UUID = Field(
+        sa_column=Column(pg.UUID, nullable=False, primary_key=True, default=uuid4)
+    )
     name: str
     phone_number: str
-    created_at: datetime = Field(default_factory=datetime.now)
-    updated_at: datetime = Field(default_factory=datetime.now)
+    created_at: datetime = Field(
+        sa_column=Column(pg.TIMESTAMP, nullable=False, default=datetime.now)
+    )
+    updated_at: datetime = Field(
+        sa_column=Column(pg.TIMESTAMP, nullable=False, default=datetime.now)
+    )
 
     def __repr__(self):
         return f"<Customer {self.name}>"
