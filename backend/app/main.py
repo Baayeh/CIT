49cbb757627bb77c2cdba56c7389053075a4d17e
Kwasi Antwi Baayeh <kabaayeh@gmail.com>
2024-12-14 04:09:57 +0000

    update main file with ticket router

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,7 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from app.db.database import engine, init_db
-from app.routers import auth_routes, customer_routes
+from app.routers import auth_routes, customer_routes, ticket_routes
 
 
 @asynccontextmanager
@@ -19,7 +19,10 @@ version = "v1"
 
 app = FastAPI(version=version)
 
+app.include_router(auth_routes.router, prefix=f"/api/{version}/auth", tags=["auth"])
 app.include_router(
     customer_routes.router, prefix=f"/api/{version}/customers", tags=["customers"]
 )
-app.include_router(auth_routes.router, prefix=f"/api/{version}/auth", tags=["auth"])
+app.include_router(
+    ticket_routes.router, prefix=f"/api/{version}/tickets", tags=["tickets"]
+)
