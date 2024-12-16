d8ca50f42b36701f00c32f296da13208b587369f
Kwasi Antwi Baayeh <kabaayeh@gmail.com>
2024-12-16 10:44:00 +0000

    add custom logging middleware

---
+++ backend/app/utils/middleware.py
@@ -0,0 +1,23 @@
+import logging
+import time
+
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
+logger = logging.getLogger("uvicorn.access")
+logger.disabled = True
+
+
+def register_middleware(app: FastAPI):
+    @app.middleware("http")
+    async def custom_logging(request: Request, call_next):
+        start_time = time.time()
+
+        response = await call_next(request)
+        process_time = time.time() - start_time
+
+        message = f"{request.client.host}:{request.client.port} - {request.method} - {request.url.path} - {response.status_code} completed after {process_time}s"
+
+        print(message)
+
+        return response
